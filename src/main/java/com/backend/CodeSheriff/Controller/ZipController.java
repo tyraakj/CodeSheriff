64d28877d453dd4b10982943e8d81f868163b3bc
tyraakj <tyra191712@gmail.com>
2026-05-05 03:15:20 +0530

    wip2/Controller code

--- src/main/java/com/backend/CodeSheriff/Controller/ZipController.java
+++ src/main/java/com/backend/CodeSheriff/Controller/ZipController.java
@@ -1,10 +1,20 @@
 package com.backend.CodeSheriff.Controller;
 
+import com.backend.CodeSheriff.Model.ClassInfo;
 import com.backend.CodeSheriff.Service.JavaParserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @RestController
 @RequestMapping("/api")
@@ -13,4 +23,37 @@ public class ZipController {
 
     @Autowired
     private JavaParserService parserService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<List<ClassInfo>> uploadZip(
+            @RequestParam("file")MultipartFile zipFile) throws IOException{
+        if (zipFile.isEmpty()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        Map<String, String> javaFiles = extractJavaFilesFromZip(zipFile);
+        List<ClassInfo> classes = parserService.parseAll(javaFiles);
+
+        return ResponseEntity.ok(classes);
+    }
+
+    private Map<String, String> extractJavaFilesFromZip(MultipartFile zipFile) throws IOException{
+        Map<String, String> result = new HashMap<>();
+
+        try(ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(zipFile.getBytes()))) {
+
+            ZipEntry entry;
+            while((entry = zis.getNextEntry()) != null){
+                if((!entry.isDirectory() && entry.getName().endsWith(".java"))){
+                    String content = new String(zis.readAllBytes(), StandardCharsets.UTF_8);
+                    result.put( entry.getName(),content);
+                }
+                zis.closeEntry();
+            }
+
+
+        }
+        return result;
+    }
+
 }
