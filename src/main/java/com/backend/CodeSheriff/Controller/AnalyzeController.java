765587942f059445764b49df9e57124c1809bc6a
tyraakj <tyra191712@gmail.com>
2026-05-05 02:20:40 +0530

    wip1/Controller code

--- src/main/java/com/backend/CodeSheriff/Controller/AnalyzeController.java
+++ src/main/java/com/backend/CodeSheriff/Controller/AnalyzeController.java
@@ -1,4 +1,37 @@
 package com.backend.CodeSheriff.Controller;
 
+import com.backend.CodeSheriff.Model.BobAnalysis;
+import com.backend.CodeSheriff.Service.BobService;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+@CrossOrigin("http://localhost:5173")
 public class AnalyzeController {
+
+    @Autowired
+    private BobService bobService;
+
+    @PostMapping("/analyze")
+    public ResponseEntity<BobAnalysis> analyzeMethod(@RequestBody AnalyzeRequest request){
+
+        BobAnalysis analysis = bobService.analyze(
+                request.getClassName(),
+                request.getMethodName(),
+                request.getMethodBody(),
+                request.getAllClassContext()
+        );
+        return ResponseEntity.ok(analysis);
+    }
+}
+
+@Data
+public class AnalyzeRequest{
+    private String className;
+    private String methodName;
+    private String methodBody;
+    private String allClassContext;
 }
