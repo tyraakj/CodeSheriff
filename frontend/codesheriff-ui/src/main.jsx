3f550091d1da28aa5b59786777c1a85758ffed48
tyraakj <tyra191712@gmail.com>
2026-05-16 20:00:31 +0530

    Protected Routes added

--- frontend/codesheriff-ui/src/main.jsx
+++ frontend/codesheriff-ui/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <ProtectedRoute>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </ProtectedRoute>
   </StrictMode>,
 );
